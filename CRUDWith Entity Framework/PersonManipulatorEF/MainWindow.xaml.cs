d769ded0f1985202249204629c0fd1dbe353c4f1
Gayane Khachatryan <kh.gaiane@gmail.com>
2017-05-21 23:59:05 +0400

    added

---
+++ CRUDWith Entity Framework/PersonManipulatorEF/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PersonManipulatorEF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Entities crud;
+
+        List<Person> personlist;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            crud = new Entities();
+
+            personlist = new List<Person>();
+        }
+
+        private void Get_All_PersonBt_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+    }
+}
