36cdae371593a3d9b7a29e90ec4cd5ce970c8eca
Gayane Khachatryan <kh.gaiane@gmail.com>
2017-05-22 01:31:25 +0400

    finished

--- CRUDWith Entity Framework/PersonManipulatorEF/MainWindow.xaml.cs
+++ CRUDWith Entity Framework/PersonManipulatorEF/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -35,7 +38,102 @@ namespace PersonManipulatorEF
 
         private void Get_All_PersonBt_Click(object sender, RoutedEventArgs e)
         {
+            RefreshPersonsList();
+        }
+
+        private async void Get_By_IdBt_Click(object sender, RoutedEventArgs e)
+        {
+            string text = GetIDTxBox.Text;
            
+            if (!string.IsNullOrEmpty(text) && text.All(char.IsDigit))
+            {
+              
+                int id = int.Parse(text);
+              
+                personlist = null;
+               
+                personlist = await crud.People.Where(p => p.Id == id).ToListAsync();
+              
+                MyGrid.ItemsSource = personlist;
+               
+                if (personlist.Count == 0)
+                {
+                    MessageBox.Show("No person with Id" + text);
+                }
+            }
+        }
+
+        private async void AddNewBt_Click(object sender, RoutedEventArgs e)
+        {
+            crud.People.Add(new Person
+            {
+                FName = FNameTxBox.Text,
+               
+                LName = LNametxtBox.Text,
+               
+                Phone = int.Parse(PhonetxBox.Text)
+            });
+
+            try
+            {
+                await crud.SaveChangesAsync();
+              
+                MessageBox.Show("New Person added");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            RefreshPersonsList();
+        }
+
+        private void UpdateBt_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyGrid.SelectedItems.Count != 1) return;
+
+            var id = personlist[MyGrid.SelectedIndex].Id;
+           
+            var person = crud.People.Find(id);
+
+            if (person == null) return;
+
+            person.FName = FNameTxBox.Text;
+           
+            person.LName = LNametxtBox.Text;
+           
+            person.Phone = int.Parse(PhonetxBox.Text);
+
+            crud.People.AddOrUpdate(person);
+           
+            crud.SaveChanges();
+
+            RefreshPersonsList();
+        }
+
+        private void DeleteBt_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyGrid.SelectedItems.Count != 1) return;
+
+            var id = personlist[MyGrid.SelectedIndex].Id;
+           
+            var person = crud.People.Find(id);
+
+            crud.People.Remove(person);
+
+            crud.SaveChanges();
+           
+            RefreshPersonsList();
+
+        }
+
+        private async void RefreshPersonsList()
+        {
+            MyGrid.ItemsSource = null;
+           
+            personlist = await crud.People.ToListAsync();
+           
+            MyGrid.ItemsSource = personlist;
         }
     }
 }
